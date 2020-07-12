3a8c3483fd5028e094575446e560d219591a3608
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-07-12 21:25:54 +0600

    Added testing unit.

---
+++ tests/PWA.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CodexShaper\PWA\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class PWA extends TestCase
+{
+    /** @test */
+    public function it_provides_a_factory_method()
+    {
+        $this->assertSame(true, true);
+    }
+}
