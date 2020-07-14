3e6e1960f5d02d1af8fc8a211d40a3d7892c241c
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-07-14 12:18:20 +0600

    Fixed router namespace issue for laravel 5.6 or above

--- routes/pwa.php
+++ routes/pwa.php
@@ -1,23 +1,36 @@
 <?php
 
-Route::namespace('\CodexShaper\PWA\Http\Controllers')->group(function () {
-    Route::get('/pwa/assets/{path?}', 'PwaController@asset')
+Route::group(['middleware' => 'web'], function(){
+    Route::get('/pwa/assets/{path?}', '\CodexShaper\PWA\Http\Controllers\PwaController@asset')
         ->where('path', '(.*)')
         ->name('pwa.asset');
-    Route::get('serviceworker', 'PwaController@serviceWorker')->name('pwa.serviceworker');
-    Route::get('register-serviceworker', 'PwaController@registerServiceWorker')->name('pwa.serviceworker.register');
-    Route::get('offline', 'PwaController@offline')->name('pwa.offline');
+    Route::get(
+        'serviceworker', 
+        '\CodexShaper\PWA\Http\Controllers\PwaController@serviceWorker'
+    )->name('pwa.serviceworker');
+    Route::get(
+        'register-serviceworker', 
+        '\CodexShaper\PWA\Http\Controllers\PwaController@registerServiceWorker'
+    )->name('pwa.serviceworker.register');
+    Route::get(
+        'offline', 
+        '\CodexShaper\PWA\Http\Controllers\PwaController@offline'
+    )->name('pwa.offline');
     Route::group(['prefix' => 'pwa'], function () {
-        Route::get('manifest', 'PwaController@manifest')->name('pwa.manifest');
+        Route::get(
+            'manifest', 
+            '\CodexShaper\PWA\Http\Controllers\PwaController@manifest'
+        )->name('pwa.manifest');
     });
 
     // PWA authorisez routes.
     Route::group(['prefix' => 'pwa', 'middleware' => 'auth'], function () {
-        Route::get('', 'PwaController@index')->name('pwa');
-        Route::post('store', 'PwaController@store')->name('pwa.store');
-        Route::put('store', 'PwaController@update')->name('pwa.update');
-        Route::delete('store', 'PwaController@destroy')->name('pwa.delete');
-        Route::post('activate', 'PwaController@activate')->name('pwa.activate');
-        Route::post('deactivate', 'PwaController@deactivate')->name('pwa.deactivate');
+        Route::get('', '\CodexShaper\PWA\Http\Controllers\PwaController@index')->name('pwa');
+        Route::post('store', '\CodexShaper\PWA\Http\Controllers\PwaController@store')->name('pwa.store');
+        Route::put('store', '\CodexShaper\PWA\Http\Controllers\PwaController@update')->name('pwa.update');
+        Route::delete('store', '\CodexShaper\PWA\Http\Controllers\PwaController@destroy')->name('pwa.delete');
+        Route::post('activate', '\CodexShaper\PWA\Http\Controllers\PwaController@activate')->name('pwa.activate');
+        Route::post('deactivate', '\CodexShaper\PWA\Http\Controllers\PwaController@deactivate')->name('pwa.deactivate');
     });
 });
+
