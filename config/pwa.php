e9ae1c08a4b7c0de409c8a9f0353dc4bbb32d9e9
CodexShaper <codexshaper@gmail.com>
2020-07-14 12:54:26 +0000

    Apply fixes from StyleCI

--- config/pwa.php
+++ config/pwa.php
@@ -3,6 +3,6 @@
 return [
     'base_domain'			        => 'localhost',
     'prefix'                => '/pwa',
-    'icons_path'			=> 'vendor/codexshaper/laravel-pwa/resources/icons',
-    'scope'					=> '.'
+    'icons_path'			         => 'vendor/codexshaper/laravel-pwa/resources/icons',
+    'scope'					            => '.',
 ];
